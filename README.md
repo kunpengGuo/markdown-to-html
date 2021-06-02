2297bda0be032261a8d6d4dd7cfca35bc3695111
john <john@11h5.com>
2021-06-02 16:15:50 +0800

    marked

---
+++ README.md
@@ -0,0 +1,6 @@
+# markdown文件转html
+
+node 版本大于 10.0.0
+
+1. npm i
+2. node trans.js 文件名
--- trans.js
+++ trans.js
@@ -1,28 +1,35 @@
+// 接收参数
+const args = process.argv.splice(1);
+if (args.length < 1) {
+  console.log('missing args, usage: date');
+  process.exit(1);
+}
+const file = args[1];
 const fs = require('fs');
 const marked = require('marked');
 // 模板文件地址
 fs.readFile('./template.html', 'utf8', (err, template)=>{  
-    if(err){  
+  if(err){  
+    throw err  
+  }else{  
+    // 源文件地址
+    fs.readFile(`./md/${file}.md`, 'utf8', (err,markContent)=>{  
+      if(err){  
         throw err  
-    }else{  
-        // 源文件地址
-        fs.readFile('./avy_app/app.md', 'utf8', (err,markContent)=>{  
-            if(err){  
-                throw err  
-            }else{  
-                // 转化好的html字符串  
-                let htmlStr = marked(markContent.toString())  
-                // 将html模板文件中的 '@markdown' 替换为html字符串  
-                template = template.replace('@markdown', htmlStr)  
-                // 将新生成的字符串template重新写入到文件中==>模板文件地址  
-                fs.writeFile('./avy_app/app.html', template, err=>{  
-                    if(err){  
-                        throw err  
-                    }else{  
-                        console.log("success")  
-                    }  
-                })  
-            }  
+      }else{  
+        // 转化好的html字符串  
+        let htmlStr = marked(markContent.toString())  
+        // 将html模板文件中的 '@markdown' 替换为html字符串  
+        template = template.replace('@markdown', htmlStr)  
+        // 将新生成的字符串template重新写入到文件中==>模板文件地址  
+        fs.writeFile(`./html/${file}.html`, template, err=>{  
+          if(err){  
+            throw err  
+          }else{  
+            console.log("success")  
+          }  
         })  
-    }  
+      }  
+    })  
+  }  
 })
